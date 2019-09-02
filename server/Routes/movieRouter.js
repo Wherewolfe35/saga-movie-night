fcb3e79711870b0e6eda9976faae1a3372cb93f0
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-09-02 14:59:20 -0500

    adds search bar with get functionality and clearing ability, limit 10

--- server/Routes/movieRouter.js
+++ server/Routes/movieRouter.js
@@ -2,19 +2,35 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-//gets full movie list from the Database for home page
+//gets searched for movie list from the Database for home page, defaults to 10 in alphebetical order
 router.get('/', (req, res) => {
   console.log('GETing movies');
-  let queryText = `SELECT * FROM "movies" ORDER BY "title" asc;`;
-  pool.query(queryText)
-    .then((result) => {
-      console.log('GOT from DB');
-      res.send(result.rows);
-    })
-    .catch((error) => {
-      console.log('unable to GET', error);
+  if (req.query.q === undefined) {
+    let queryText = `SELECT * FROM "movies" ORDER BY "title" asc LIMIT 10;`;
+    pool.query(queryText)
+      .then((result) => {
+        console.log('GOT from DB');
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.log('unable to GET', error);
+        res.sendStatus(500);
+      })
+  } else {
+    let search = '%' + req.query.q + '%';
+    console.log('searching for', search);
+    let queryText = `SELECT * FROM "movies" 
+      WHERE "title" ILIKE $1 OR "description" ILIKE $1
+      ORDER BY "title" asc Limit 10;`;
+    pool.query(queryText, [search])
+    .then((results) => {
+      console.log('Search successful', results.rows);
+      res.send(results.rows);
+    }).catch((error) => {
+      console.log('Unable to GET', error);
       res.sendStatus(500);
     })
+  }
 })
 
 //gets movie details for selected movie for details page
@@ -27,13 +43,13 @@ router.get('/:id', (req, res) => {
   WHERE "movies".id = $1
   GROUP BY "movies".id;`;
   pool.query(queryText, [req.params.id])
-  .then((result) => {
-    console.log('GOT from DB');
-    res.send(result.rows);
-  })
-  .catch((error) => {
-    console.log('unable to get details', error);
-  })
+    .then((result) => {
+      console.log('GOT from DB');
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('unable to get details', error);
+    })
 })
 
 //updates movies table with new title and description
@@ -41,15 +57,15 @@ router.put('/', (req, res) => {
   console.log('UPDATEing', req.body);
   let queryText = `UPDATE "movies" SET "title" = $1, "description" = $2
 WHERE "id" = $3;`;
-pool.query(queryText, [req.body.title, req.body.description, req.body.id])
-.then((result) => {
-  console.log('Update complete');
-  res.sendStatus(200);
-})
-.catch((error) => {
-  console.log('Update failed', error);
-  res.sendStatus(500);
-})
+  pool.query(queryText, [req.body.title, req.body.description, req.body.id])
+    .then((result) => {
+      console.log('Update complete');
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('Update failed', error);
+      res.sendStatus(500);
+    })
 })
 
 
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -17,7 +17,7 @@ class MovieDetails extends Component {
       <section>{/* Using conditional rendering here because the page tries to load before current details is set. 
       I'm sure this can be fixed using saga, but not sure how*/}
         {this.props.currentDetails !== '' && this.props.currentDetails.map(details => <div key={details.id}>
-          <h1>{details.title} Details <span>{details.genres.map(genre => <span key={genre}> {genre}</span>)}
+          <h1>{details.title} Details <span>{details.genres.map(genre => <span key={genre}> {genre} &nbsp;</span>)}
           </span>
           </h1>
           <img src={details.poster} alt={details.title} />
